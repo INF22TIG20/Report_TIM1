d3c45566c6efceb53ff3f4d30c0c87adb13ef1bb
CatiaAntunes <catiafantunes@gmail.com>
2023-06-11 15:20:50 +0100

    +list and +table
    Inserção de uma lista e de uma tabela, de acordo com os requisitos
    A lista está a ser gerada por javascript, obtendo a informação de um ficheiro xml
    Ajuste de css para a lista, em falta para a table

---
+++ src/js/faq.js
@@ -0,0 +1,45 @@
+document.getElementById('faq').addEventListener('click', function() {
+    let resultDiv = document.getElementById('result');
+    
+    // If the resultDiv is not empty, clear it and return
+    if (resultDiv.innerHTML.trim() !== "") {
+        resultDiv.innerHTML = "";
+        return;
+    }
+    
+    // If the resultDiv is empty, fetch the data and populate it
+    fetch('/src/xml/faq/faq.xml')
+        .then(response => response.text())
+        .then(data => {
+            let parser = new DOMParser();
+            let xml = parser.parseFromString(data, "application/xml");
+            
+            let faqis = xml.getElementsByTagName('faqi');
+            let result = '';
+
+            for(let i = 0; i < faqis.length; i++) {
+                let pergunta = faqis[i].getElementsByTagName('pergunta')[0].textContent;
+                let resposta = faqis[i].getElementsByTagName('resposta')[0].textContent;
+
+                result += `<dt><span class="qa">Q.</span> ${pergunta}</dt><dd><span class="qa">A.</span> ${resposta}</dd>`;
+            }
+
+            resultDiv.innerHTML = `<dl>${result}</dl>`;
+
+            // Attach click event listener to each question
+            let questions = resultDiv.getElementsByTagName('dt');
+            for (let i = 0; i < questions.length; i++) {
+                questions[i].addEventListener('click', function() {
+                    let answer = this.nextElementSibling;
+                    if (answer.style.display === 'none') {
+                        answer.style.display = 'block';
+                    } else {
+                        answer.style.display = 'none';
+                    }
+                });
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+});
